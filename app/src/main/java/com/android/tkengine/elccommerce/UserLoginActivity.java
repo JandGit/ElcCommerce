6f36dc1895b1ed767ebbd9a7bb2de3e702cfc8d6
JandGit <15521234972@163.com>
2016-08-09 22:07:14 +0800

    刘俊杰：2016-8-9, 22:07

--- app/src/main/java/com/android/tkengine/elccommerce/UserLoginActivity.java
+++ app/src/main/java/com/android/tkengine/elccommerce/UserLoginActivity.java
@@ -1,13 +1,25 @@
 package com.android.tkengine.elccommerce;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 public class UserLoginActivity extends AppCompatActivity {
+
+    Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_login);
+
+        findViewById(R.id.iv_back).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
