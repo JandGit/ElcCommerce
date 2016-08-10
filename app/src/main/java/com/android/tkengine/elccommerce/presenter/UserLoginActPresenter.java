fbfeb721aa5b4aa07ed1876b7388c23d29afe280
JandGit <15521234972@163.com>
2016-08-10 19:43:27 +0800

    刘俊杰：2016-8-10,

--- app/src/main/java/com/android/tkengine/elccommerce/presenter/UserLoginActPresenter.java
+++ app/src/main/java/com/android/tkengine/elccommerce/presenter/UserLoginActPresenter.java
@@ -2,6 +2,7 @@ package com.android.tkengine.elccommerce.presenter;
 
 
 import android.content.Context;
+import android.graphics.Bitmap;
 
 public class UserLoginActPresenter {
 
@@ -13,10 +14,15 @@ public class UserLoginActPresenter {
     public interface CallbackOfView{
         //登录成功后的操作
         void onLoginSuccess();
+
         //登录失败后的操作
         void onLoginFailed();
+
         //在Acitivity展示消息
         void showToast(String text);
+
+        //在页面显示用户头像
+        void setUserIcon(Bitmap icon);
     }
     public interface CallbackOfModel{
 
@@ -32,10 +38,16 @@ public class UserLoginActPresenter {
             mView.showToast("用户名或密码不能为空");
             return;
         }
-        if(userName.matches("^[1][358][0-9]{9}$")){
+        if(!userName.matches("^[1][358][0-9]{9}$")){
             mView.showToast("手机号码格式不正确");
             return;
         }
 
+    }
+
+    public void loadUserIcon(String userName){
+        if(userName != null && userName.matches("^[1][358][0-9]{9}$")){
+
+        }
     }
 }
