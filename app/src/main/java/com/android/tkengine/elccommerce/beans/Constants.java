5153f8a131f54a6cf02385222607334a00fe4dec
JandGit <15521234972@163.com>
2016-08-11 12:48:35 +0800

    刘俊杰：2016-8-10,

--- app/src/main/java/com/android/tkengine/elccommerce/beans/Constants.java
+++ app/src/main/java/com/android/tkengine/elccommerce/beans/Constants.java
@@ -4,10 +4,21 @@ package com.android.tkengine.elccommerce.beans;
  * 用于储存一些常量
  */
 public class Constants {
-    //后台服务器地址
+    /**
+     * 后台服务器地址
+     */
     public static final String SERVER_ADDRESS = "http://192.168.1.102:8080/TKBaas/";
-    //后台用户登录接口
+    /**
+     * 后台用户登录接口
+     */
     public static final String SERVER_ADDRESS_LOGIN = "http://192.168.1.102:8080/TKBaas/user/app/login";
-    //获取用户信息的接口
+    /**
+     * 获取用户信息的接口
+     */
     public static final String SERVER_GETUSERINFO = "http://192.168.1.102:8080/TKBaas/user/app/getInfo";
+    /**
+     * 获取用户购物车
+     * 调用时向服务器Post数据:user_id = XXX
+     */
+    public static final String SERVER_GETCART = "http://192.168.1.107:8080/TKBaas/cart/app/getUserProduct";
 }
