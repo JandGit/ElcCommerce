7a8f63e9ce00af3d66bb3de040de3dff795e96bd
JandGit <15521234972@163.com>
2016-08-12 21:21:59 +0800

    刘俊杰：2016-8-10,

---
+++ app/src/main/java/com/android/tkengine/elccommerce/UI/OrderActivity.java
@@ -0,0 +1,15 @@
+package com.android.tkengine.elccommerce.UI;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.android.tkengine.elccommerce.R;
+
+public class OrderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order);
+    }
+}
